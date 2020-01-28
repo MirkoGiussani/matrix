e48823cb27f9d6d5cc22d1d606290c7e1184b93e
Mirko <Mirko@192.168.1.5>
2020-01-28 16:17:31 +0100

    add a comments

--- matrix4BI/src/matrix/Matrix.java
+++ matrix4BI/src/matrix/Matrix.java
@@ -14,35 +14,37 @@ public class Matrix {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(new File("matrix.txt"));
-        int rows = 0;
-        int columns = 1;
+        int righe = 0;
+        int colonne = 1;
         
-        while (input.hasNextLine()) {
-            columns = 1;
-            ++rows;
-            String riga = input.nextLine();
+        while (input.hasNextLine()) {   //ciclo finchè c'è una riga
+            colonne = 1;
+            ++righe;
+            String riga = input.nextLine(); //prende la linea dal file
             for (int i = 0; i < riga.length(); i++) {
+                //conta gli spazi presenti in una riga e aggiungendo 1 trova le colonne
                 if (riga.charAt(i) == ' ') {
-                    ++columns;
+                    ++colonne;
                 }
             }
         }
         
-        System.out.println("matrix has " + rows + " rows and " + columns + " columns");
-        int[][] a = new int[rows][columns];
+        System.out.println("la matrice ha " + righe + " righe e " + colonne + " colonne");
+        int[][] a = new int[righe][colonne];    //creo una matrice con righe e colonne adeguate
 
-        input = new Scanner(new File("matrix.txt"));
+        input = new Scanner(new File("matrix.txt"));    /* non ho capito a cosa serve */
 
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
-                if (input.hasNextInt()) {
-                    a[i][j] = input.nextInt();
+        for (int i = 0; i < righe; ++i) {
+            for (int j = 0; j < colonne; ++j) {
+                if (input.hasNextInt()) {   //se il carattere è un intero
+                    a[i][j] = input.nextInt();  //inserisce il carattere scansionato nella matrice
                 }
             }
         }
 
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < columns; ++j) {
+        //stampa matrice
+        for (int i = 0; i < righe; ++i) {
+            for (int j = 0; j < colonne; ++j) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.print("\n");
